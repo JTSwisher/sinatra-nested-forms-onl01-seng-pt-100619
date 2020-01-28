780d91dcc86cf9ff8b8ed6a98409ba821373fd21
Jeff Swisher <jefftswisher@gmail.com>
2020-01-28 22:49:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,30 @@
 require './environment'
 
 module FormsLab
+  
   class App < Sinatra::Base
-
-    # code other routes/actions here
-
+    
+    get '/' do 
+      erb :'pirates/index'
+    end 
+    
+    post '/pirates' do 
+      
+      binding.pry
+    end 
+    
+    get '/show' do 
+      
+      erb :'pirates/show'
+    end 
+    
+    get '/new' do
+      erb :'pirates/new'
+    end 
+  
+  
+  
   end
+  
+  
 end
